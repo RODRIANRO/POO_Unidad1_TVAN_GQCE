e5d8e2b0c72e6d5a0a598e209891c0017eaca04a
NOTEBOOK <NOTEBOOK@172.16.213.125>
2022-04-07 20:34:05 -0500

    MAVE01

---
+++ src/main/java/com/mycompany/maven/Empleado.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.maven;
+
+/**
+ *
+ * @author NOTEBOOK
+ */
+public class Empleado {
+    String nombre;
+    int horasTrabajadas;
+    double costoHoras;
+    int anoIngreso;
+    
+}
