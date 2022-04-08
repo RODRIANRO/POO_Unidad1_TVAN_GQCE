dfbc0bcafada115ecce823e85e6e7f84776e4a5e
NOTEBOOK <NOTEBOOK@172.16.213.125>
2022-04-08 08:55:36 -0500

    MAVEN02

--- src/main/java/com/mycompany/maven/Empleado.java
+++ src/main/java/com/mycompany/maven/Empleado.java
@@ -9,25 +9,48 @@ package com.mycompany.maven;
  * @author NOTEBOOK
  */
 public class Empleado {
+    double costoHora;
+    int anoIngreso;
     String nombre;
     int horasTrabajadas;
-    double costoHoras;
-    int anoIngreso;
-    int anoActual;
     
-     public int anoYear(int currendYear )
-     {
-          var retorno=1000;
-           retorno=currendYear-this.anoIngreso;
-          return retorno;
-     }
-     
-     public double calculoIngresos ( int currentYear, int anoIngreso){
-       var retorno=100000d;
-       retorno=((0.02)*(currentYear-anoIngreso)*(costoHoras*horasTrabajadas)+(costoHoras*horasTrabajadas));
-       
-       
-     return retorno;
-     
-     }
+    public double calculoIngreso(int añoActual){
+        var retorno=100000d;
+        if(this.anoIngreso<añoActual){
+            retorno=(this.costoHora*this.horasTrabajadas)*((añoActual-this.anoIngreso)*0.02)+(this.costoHora*this.horasTrabajadas);
+        }else{
+            retorno=(this.costoHora*this.horasTrabajadas);
+        }
+        return retorno;      
+    }
+    public double calcularBonoHorasExtra(int maxHorasTrabajadas){
+        var retorno= 1000000d;
+        if(this.horasTrabajadas>=maxHorasTrabajadas){
+            retorno=(this.horasTrabajadas-maxHorasTrabajadas)*(this.costoHora*2);
+        }else{
+            retorno=0;
+        }
+        return retorno;
+    }
+    public double calculoImpuesto(int limite1, int limite2, int limite3){
+        var retorno=100000d;
+        double r;
+        r=this.costoHora*this.horasTrabajadas;
+        if(r>0 && r<limite1){
+            retorno=0;
+        }else{
+            if(r>=limite1 && r<limite2){
+                retorno=this.costoHora*this.horasTrabajadas*0.05;
+            }else{
+                if(r>=limite2 && r<=limite3){
+                    retorno=this.costoHora*this.horasTrabajadas*0.12;
+                }else{
+                    if(r>limite3){
+                        retorno=this.costoHora*this.horasTrabajadas*0.25;
+                    }
+                }      
+            }
+        }
+        return retorno;
+    }
 }
--- src/main/java/com/mycompany/maven/MAVEN.java
+++ src/main/java/com/mycompany/maven/MAVEN.java
@@ -11,22 +11,67 @@ package com.mycompany.maven;
  */
 public class MAVEN {
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
         System.out.println("Hello World!");
+       
+        var empleadoNuevo= new Empleado();
+        empleadoNuevo.nombre="Julian";
+        empleadoNuevo.horasTrabajadas=100;
+        empleadoNuevo.costoHora=6;
+        empleadoNuevo.anoIngreso=2010;  
         
-        var empleadoNuevo = new Empleado();
-        empleadoNuevo.nombre="Juan";
-        empleadoNuevo.costoHoras= 6;
-        empleadoNuevo.horasTrabajadas= 100;
-        empleadoNuevo.anoIngreso= 2010;
-        empleadoNuevo.anoActual=2022;
+        System.out.println( empleadoNuevo.nombre+" | "+
+                            empleadoNuevo.horasTrabajadas+" | "+
+                            empleadoNuevo.costoHora+" | "+
+                            empleadoNuevo.anoIngreso+" | ");
         
+        var ingresos=empleadoNuevo.calculoIngreso(2022);
+        System.out.println("El valor de ingresos es: "+ingresos);
+        var bono=empleadoNuevo.calcularBonoHorasExtra(150);
+        System.out.println("El valor del bono es: "+bono);
+        var impuestos=empleadoNuevo.calculoImpuesto(500, 1000, 2000);
+        System.out.println("El impuesto es: "+empleadoNuevo.calculoImpuesto(500, 1000, 2000));
+        var resta=ingresos+bono-impuestos;
+        System.out.println("El valor total a pagar es: "+resta);
         
-        System.out.println("Nombre :"+ empleadoNuevo.nombre + " ingreso hace :" + empleadoNuevo.anoYear(2020)
-                            + "años en la enpresa");
+        var empleadoNuevo2= new Empleado();
+        empleadoNuevo2.nombre="Marta";
+        empleadoNuevo2.horasTrabajadas=200;
+        empleadoNuevo2.costoHora=5;
+        empleadoNuevo2.anoIngreso=2020;  
+        System.out.println( empleadoNuevo2.nombre+" | "+
+                            empleadoNuevo.horasTrabajadas+" | "+
+                            empleadoNuevo2.costoHora+" | "+
+                            empleadoNuevo2.anoIngreso+" | "); 
         
-        System.out.println(" Su Ingreso es:"+empleadoNuevo.calculoIngresos(2022, 2010));
-      
+        var ingresos2=empleadoNuevo2.calculoIngreso(2022);
+        System.out.println("El valor de ingresos es: "+ingresos2);
+        var bono2=empleadoNuevo2.calcularBonoHorasExtra(100);
+        System.out.println("El valor del bono es: "+bono2);
+        var impuestos2=empleadoNuevo2.calculoImpuesto(500, 1000, 2000);
+        System.out.println("El impuesto es: "+empleadoNuevo2.calculoImpuesto(500, 1000, 2000));
+        var resta2=ingresos2+bono2-impuestos2;
+        System.out.println("El valor total a pagar es: "+resta2);
+        
+        var empleadoNuevo3= new Empleado();
+        empleadoNuevo3.nombre="Esteban";
+        empleadoNuevo3.horasTrabajadas=150;
+        empleadoNuevo3.costoHora=8;
+        empleadoNuevo3.anoIngreso=2018;  
+        
+        System.out.println( empleadoNuevo3.nombre+" | "+
+                            empleadoNuevo3.horasTrabajadas+" | "+
+                            empleadoNuevo3.costoHora+" | "+
+                            empleadoNuevo3.anoIngreso+" | ");
+        
+        var ingresos3=empleadoNuevo3.calculoIngreso(2022);
+        System.out.println("El valor de ingresos es: "+ingresos3);
+        var bono3=empleadoNuevo3.calcularBonoHorasExtra(100);
+        System.out.println("El valor del bono es: "+bono3);
+        var impuestos3=empleadoNuevo3.calculoImpuesto(500, 1000, 2000);
+        System.out.println("El impuesto es: "+empleadoNuevo.calculoImpuesto(500, 1000, 2000));
+        var resta3=ingresos3+bono3-impuestos3;
+        System.out.println("El valor total a pagar es: "+resta3);
         
     }
 }
